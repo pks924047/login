89d5623c4516b6c0429a9f557c25edf98ddb640b
Palak Saxena <pks924047@gmail.com>
2022-08-07 21:38:02 +0530

    login commit

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,16 @@
+import { Grid } from "@mui/material";
+
+const Contact = () => {
+  return <>
+    <Grid container justifyContent='center'>
+      <Grid item sm={10}>
+        <h1>Contact Page</h1>
+        <hr />
+        <p> ANY PROBLEM SO PLEASE CONTACT </p>
+        <p>Contact Number - 9719905214</p>
+      </Grid>
+    </Grid>
+  </>;
+};
+
+export default Contact;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import { Grid } from "@mui/material";
+
+const Home = () => {
+  return <>
+    <Grid container justifyContent='center'>
+      <Grid item sm={10}>
+        <h1>Home Page</h1>
+        <hr />
+        <p>Created by Palak Saxena.</p>
+      </Grid>
+    </Grid>
+  </>;
+};
+
+export default Home;
